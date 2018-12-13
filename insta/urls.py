ff25a055394051af3a8d34faf1dc91d315120f5b
odipojames <odipojames12@gmai.com>
2018-12-13 23:01:20 +0300

    create base.html file

--- insta/urls.py
+++ insta/urls.py
@@ -19,7 +19,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('app.urls')),
+    url(r'',include('instagram.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/accounts/login/'}),
     url(r'^tinymce/', include('tinymce.urls')),
