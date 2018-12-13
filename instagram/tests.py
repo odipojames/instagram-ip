4bf416bfbe8371e2c9d4938b4b8ab1cc80563a4d
odipojames <odipojames12@gmai.com>
2018-12-13 23:58:14 +0300

    test codes

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,66 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+
+class ImageTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(id = 1, username='j')
+        self.profile = Profile.objects.create(name = self.user,bio = 'bb')
+        self.likes = Like.objects.create(likes = 1)
+        self.image = Image.objects.create(image_name = 'kk',
+                                          id = 1,
+                                          image_caption ='ww',
+                                          likes = self.likes,
+                                          profile = self.profile)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))
+
+    def test_delete(self):
+        self.image.save()
+        self.image.delete()
+        self.assertTrue(len(Image.objects.all()) == 0)
+
+    def test_get_image(self):
+        self.image.save()
+        image = Image.get_image(1)
+        self.assertTrue(len(image) == 1)
+
+    def test_get_post(self):
+        self.image.save()
+        image = Image.get_post('j')
+        self.assertTrue(len(image) == 1)
+
+class ProfileTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(id = 1, username='j')
+        self.profile = Profile.objects.create(name = self.user,bio = 'bb')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    def test_get_profile(self):
+        self.profile.save()
+        profile = Profile.get_profile('j')
+        self.assertTrue(len(profile)== 1)
+
+    def test_delete(self):
+        self.profile.save()
+        self.profile.delete()
+        self.assertTrue(len(Profile.objects.all()) == 0)
+
+    def test_search_profile(self):
+        self.profile.save()
+        profile = Profile.search_profile('j')
+        self.assertTrue(len(profile)== 1)
+
+class likesTest(TestCase):
+
+    def setUp(self):
+        self.likes = Like.objects.create(likes=1)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.likes,Like))
--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,101 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import *
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .forms import *
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    '''
+    function to return the homepage of the application
+    '''
+    pic = Image.objects.all()
+    return render(request,'index.html',{'pic': pic})
+
+def image(request,pic_id):
+    '''
+    function that returns pages to store individual images
+    '''
+    pic = Image.get_image(id=pic_id)
+    return render(request,'image.html',{'item': pic})
+
+def profile(request,iden):
+    '''
+    function that returns individual profiles
+    '''
+    person = Profile.get_profile(identity=iden)
+    posts = Image.get_post(jina=iden)
+    return render(request,'profile.html',{'human':person,'posts':posts})
+
+@login_required(login_url='/accounts/login/')
+def new_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.name = current_user
+            profile.save()
+        return redirect('home')
+
+    else:
+        form = EditProfileForm()
+
+    return render(request,'new_profile.html',{"form":form})
+
+@login_required(login_url='/accounts/login/')
+def comment(request,id):
+    current_user = request.user
+    image = Image.get_image(id=id)
+    item = get_object_or_404(Image, pk= id)
+    if request.method == 'POST':
+        form = NewCommentForm(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post_by = current_user
+            comment.post = item
+            comment.save()
+        return redirect('home')
+
+    else:
+        form = NewCommentForm()
+
+    return render(request,'new_comment.html',{"form": form,"images":image})
+
+@login_required(login_url='/accounts/login/')
+def post(request):
+    if request.method == 'POST':
+        form = NewImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.save()
+        return redirect('home')
+
+    else:
+        form = NewImageForm()
+
+    return render(request, 'new_image.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def search(request):
+    '''
+    function that returns the search page and the searched items
+    '''
+    if 'name' in request.GET and request.GET['name']:
+        jina = request.GET.get('name')
+        profile = Profile.search_profile(jina)
+
+        title = 'Search'
+        return render(request,'search.html',{'title':title, 'content': profile})
+
+    else:
+        return render(request,'search.html')
+
+@login_required(login_url="/accounts/login/")
+def like(request,operation,pk):
+   image = get_object_or_404(Image,pk=pk)
+   if operation == 'like':
+       image.likes.likes += 1
+       image.save()
+       return redirect('home')
